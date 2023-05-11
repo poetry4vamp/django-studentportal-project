d749006b4b2a02a3ec3c6739fcc97a3a160aee57
poetry4vamp <133098907+poetry4vamp@users.noreply.github.com>
2023-05-11 19:07:46 +0800

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -162,3 +162,18 @@ def books(request):
     context  = {'form':form}
     return render(request,"dashboard/books.html",context)
 
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Account Created for {username}.")
+            return redirect("login")
+    else:
+        form = UserRegistrationForm()
+    context = {
+        'form':form
+    }
+    return render(request,"dashboard/register.html", context)
+
