d2b8e4d81463ee27d9590122bcebe064651d418f
poetry4vamp <133098907+poetry4vamp@users.noreply.github.com>
2023-05-11 19:09:00 +0800

    Update urls.py

--- studentstudyportal/urls.py
+++ studentstudyportal/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from dashboard import views as dash_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
